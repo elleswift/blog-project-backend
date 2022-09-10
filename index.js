042bc632053c91c040448d9880b2ddbd6c3aa007
elleswift <105027856+elleswift@users.noreply.github.com>
2022-09-10 13:57:32 -0400

    updates to backend

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(PORT, () => console.log("Listening on 4000"))
+app.listen(process.env.PORT, () => console.log("Listening on 4000"))
 
-app.get('/', async (req, res) => {                    
+app.get('/get-articles', async (req, res) => {                    
 const allArticles = await articles.find().toArray()
 res.json(allArticles)
 })
 
-app.post('/', async (req, res) => {
+app.post('/add-article', async (req, res) => {
  await articles.insertOne(req.body)
   res.json('Entry added')
 })
 
-app.delete('/', async (req, res) => {
+app.delete('/remove-article', async (req, res) => {
   console.log(req.query)
   await articles.findOneAndDelete(req.query)
  res.json('Entry deleted')
@@ -35,7 +35,10 @@ app.delete('/', async (req, res) => {
  
 
 
-app.put('/', async (req, res) => {
+app.put('/update-article', async (req, res) => {
+  // find Article that you wnat to update
+  // set desried key to variable
+  // set the req.boby.title to the article title
   articles.findOneAndUpdate(req.query, { $set: req.body })
    res.json('Entry updated')
 })  
